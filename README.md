f7d638cfe797765c0d6adc03e6308a77823756d3
gopalakrishnanreddy <gopalakrishnan.reddy@gmail.com>
2020-02-18 15:26:38 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-x8hetb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-x8hetb)
---
+++ src/app/core/base-datatable/basedatatablecomponent/basedatatablecomponent.component.ts
@@ -0,0 +1,40 @@
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+
+@Component({
+  selector: 'app-datatable.base',
+  template: `base.datatable.component.html`,
+  styleUrls: ['./base.datatable.component.css']
+})
+
+export class BaseDataTableComponent implements OnInit, AfterViewInit {
+  rows: Array<any> = [];
+  columnsToDisplay: Array<any> = [];
+  requestJSON: string;
+  dataSource: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor() {
+    this.dataSource = new MatTableDataSource(this.rows);
+  }
+
+  ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+
+}
+
+
